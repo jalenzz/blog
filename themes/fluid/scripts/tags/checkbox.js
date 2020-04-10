42cf37ea2eaf9d3dc6144f1389f648b8b8dd759a
JalenChuh <JalenChuh@gmail.com>
2020-04-10 14:28:33 +0800

    :art: Tag 插件支持复选框

---
+++ themes/fluid/scripts/tags/checkbox.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function checkbox(args) {
+  args = args[0] === ',' ? args.slice(1) : args;
+  args = args.join(' ').split(',');
+  let text = args[0] || '';
+  let checked = args[1] === '1' && args[1].trim() !== 'false';
+  let special = args[1] === '2' && args[1].trim() !== 'false';
+  let color = args[2] || '';
+  let inline = (args[3] || '').length > 0 && args[2].trim() !== 'false';
+
+  return `${ !inline ? '<div>' : '' }
+            <input class="${color} ${special ? 'indeterminate' : ''}" type="checkbox" ${ checked ? 'checked="checked"' : '' }>${ text }</input>
+          ${ !inline ? '</div>' : '' }`;
+}
+
+// {% cb text, checked?, inline? %}
+hexo.extend.tag.register('checkbox', checkbox, { ends: false });
+hexo.extend.tag.register('cb', checkbox, { ends: false });
