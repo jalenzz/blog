30589446eb80ac0b1511c5027e880f0ba13ede42
JalenChuh <JalenChuh@gmail.com>
2020-04-11 20:31:25 +0800

    :sparkles: 增加检测最新版本功能

--- themes/fluid/scripts/tags/group-image.js
+++ themes/fluid/scripts/tags/group-image.js
@@ -14,27 +14,27 @@ const LAYOUTS = {
     1: [1, 2, 1],
     2: [1, 3],
     3: [2, 2],
-    4: [3, 1]
+    4: [3, 1],
   },
   5: {
     1: [1, 2, 2],
     2: [2, 1, 2],
     3: [2, 3],
-    4: [3, 2]
+    4: [3, 2],
   },
   6: {
     1: [1, 2, 3],
     2: [1, 3, 2],
     3: [2, 1, 3],
     4: [2, 2, 2],
-    5: [3, 3]
+    5: [3, 3],
   },
   7: {
     1: [1, 2, 2, 2],
     2: [1, 3, 3],
     3: [2, 2, 3],
     4: [2, 3, 2],
-    5: [3, 2, 2]
+    5: [3, 2, 2],
   },
   8: {
     1: [1, 2, 2, 3],
@@ -43,7 +43,7 @@ const LAYOUTS = {
     4: [2, 2, 2, 2],
     5: [2, 3, 3],
     6: [3, 2, 3],
-    7: [3, 3, 2]
+    7: [3, 3, 2],
   },
   9: {
     1: [1, 2, 3, 3],
@@ -52,7 +52,7 @@ const LAYOUTS = {
     4: [2, 2, 3, 2],
     5: [2, 3, 2, 2],
     6: [3, 2, 2, 2],
-    7: [3, 3, 3]
+    7: [3, 3, 3],
   },
   10: {
     1: [1, 3, 3, 3],
@@ -61,24 +61,24 @@ const LAYOUTS = {
     4: [2, 3, 3, 2],
     5: [3, 2, 2, 3],
     6: [3, 2, 3, 2],
-    7: [3, 3, 2, 2]
-  }
+    7: [3, 3, 2, 2],
+  },
 };
 
-function groupBy(group, data) {
+const groupBy = (group, data) => {
   const r = [];
   for (let count of group) {
     r.push(data.slice(0, count));
     data = data.slice(count);
   }
   return r;
-}
+};
 
 const templates = {
 
-  dispatch: function (pictures, group, layout) {
+  dispatch: (images, group, layout) => {
     const rule = LAYOUTS[group] ? LAYOUTS[group][layout] : null;
-    return rule ? this.getHTML(groupBy(rule, pictures)) : templates.defaults(pictures);
+    return rule ? this.getHTML(groupBy(rule, images)) : templates.defaults(images);
   },
 
   /**
@@ -88,55 +88,57 @@ const templates = {
    * □ □ □
    * ...
    *
-   * @param pictures
+   * @param images
    */
-  defaults: function(pictures) {
+  defaults: (images) => {
     const ROW_SIZE = 3;
-    const rows = pictures.length / ROW_SIZE;
-    const pictureArr = [];
+    const rows = images.length / ROW_SIZE;
+    const imageArr = [];
 
     for (let i = 0; i < rows; i++) {
-      pictureArr.push(pictures.slice(i * ROW_SIZE, (i + 1) * ROW_SIZE));
+      imageArr.push(images.slice(i * ROW_SIZE, (i + 1) * ROW_SIZE));
     }
 
-    return this.getHTML(pictureArr);
+    return this.getHTML(imageArr);
   },
 
-  getHTML: function(rows) {
+  getHTML: (rows) => {
     const rowHTML = rows.map(row => {
-      return `<div class="group-picture-row">${ this.getColumnHTML(row) }</div>`;
+      return `<div class="group-image-row">${ this.getColumnHTML(row) }</div>`;
     }).join('');
 
-    return `<div class="group-picture-container">${rowHTML}</div>`;
+    return `<div class="group-image-container">${ rowHTML }</div>`;
   },
 
-  getColumnHTML: function(pictures) {
-    const columnWidth = 100 / pictures.length;
+  getColumnHTML: (images) => {
+    const columnWidth = 100 / images.length;
     const columnStyle = `style="width: ${ columnWidth }%;"`;
-    return pictures.map(picture => {
-      return `<div class="group-picture-column" ${ columnStyle }>${ picture }</div>`;
+    return images.map(image => {
+      return `<div class="group-image-column" ${ columnStyle }>${ image }</div>`;
     }).join('');
-  }
+  },
 };
 
-function groupPicture(args, content) {
+const groupImage = (args, content) => {
   args = args[0].split('-');
   const group = parseInt(args[0], 10);
   const layout = parseInt(args[1], 10);
 
   content = hexo.render.renderSync({ text: content, engine: 'markdown' });
 
-  const pictures = content.match(/<img[\s\S]*?>/g);
+  const images = content.match(/<img[\s\S]*?>/g);
 
-  return `<div class="group-picture">${ templates.dispatch(pictures, group, layout) }</div>`;
-}
+  return `<div class="group-image">${ templates.dispatch(images, group, layout) }</div>`;
+};
 
 /*
-  {% grouppicture 3-x %}
+  {% groupimage 3-x %}
   ![]()
   ![]()
   ![]()
-  {% endgrouppicture %}
+  {% endgroupimage %}
  */
-hexo.extend.tag.register('grouppicture', groupPicture, {ends: true});
-hexo.extend.tag.register('gp', groupPicture, {ends: true});
+hexo.extend.tag.register('grouppicture', groupImage, { ends: true });
+hexo.extend.tag.register('gp', groupImage, { ends: true });
+hexo.extend.tag.register('groupimage', groupImage, { ends: true });
+hexo.extend.tag.register('gi', groupImage, { ends: true });
