18dfa178cb4fc90ce607a04f5a5ebca57bc94e46
JalenChuh <JalenChuh@gmail.com>
2020-04-11 20:31:25 +0800

    :sparkles: 增加检测最新版本功能

--- themes/fluid/scripts/tags/radio.js
+++ themes/fluid/scripts/tags/radio.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function radio(args) {
+const radio = (args) => {
   args = args[0] === ',' ? args.slice(1) : args;
   args = args.join(' ').split(',');
   let text = args[0] || '';
@@ -12,7 +12,7 @@ function radio(args) {
             <input class="${color}" type="radio" ${ checked ? 'checked="checked"' : '' }></input>
             ${ hexo.render.renderSync({ text: text, engine: 'markdown' }).split('\n').join('') }
           ${ !inline ? '</div>' : '' }`;
-}
+};
 
 // {% rd text, checked?, inline? %}
 hexo.extend.tag.register('rd', radio, { ends: false });
