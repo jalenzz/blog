cfb39b9a820ac0b44790a148cea54b37e42f7bd0
Royce2019 <1098870059@qq.com>
2020-04-14 08:37:51 +0800

    :bug: 修复高亮相关问题

--- themes/fluid/source/js/clipboard-use.js
+++ themes/fluid/source/js/clipboard-use.js
@@ -1,33 +1,47 @@
 !function (e, t, a) {
-    var initCopyCode = function () {
-      var copyHtml = '';
-      copyHtml += '<button class="copy-btn" data-clipboard-snippet="">';
-      copyHtml += '<i class="iconfont icon-copy"></i><span>Copy</span>';
-      copyHtml += '</button>';
-      $('.markdown-body pre').each(function () {
-        var pre = $(this);
-        if (pre.find('code.mermaid').length > 0) {
-          return;
-        }
-        pre.append(copyHtml);
-      });
-      var clipboard = new ClipboardJS('.copy-btn', {
-        target: function (trigger) {
-          return trigger.previousElementSibling;
-        },
-      });
-      clipboard.on('success', function (e) {
-        e.clearSelection();
-        e.trigger.innerHTML = 'Success';
-        setTimeout(function () {
-          e.trigger.outerHTML = copyHtml;
-        }, 2000);
-      });
-    };
-    var oldLoadCb = window.onload;
-    window.onload = function () {
-      oldLoadCb && oldLoadCb();
+  function initCopyCode() {
+    var copyHtml = '';
+    copyHtml += '<button class="copy-btn" data-clipboard-snippet="">';
+    copyHtml += '<i class="iconfont icon-copy"></i><span>Copy</span>';
+    copyHtml += '</button>';
+    $('.markdown-body pre').each(function () {
+      var pre = $(this);
+      if (pre.find('code.mermaid').length > 0) {
+        return;
+      }
+      pre.append(copyHtml);
+    });
+    var clipboard = new ClipboardJS('.copy-btn', {
+      target: function (trigger) {
+        return trigger.previousElementSibling;
+      },
+    });
+    $('.copy-btn').addClass(getBgClass());
+    clipboard.on('success', function (e) {
+      e.clearSelection();
+      var tmp = e.trigger.outerHTML;
+      e.trigger.innerHTML = 'Success';
+      setTimeout(function () {
+        e.trigger.outerHTML = tmp;
+      }, 2000);
+    });
+  }
 
-      initCopyCode();
-    };
-  }(window, document);
+  function getBgClass() {
+    var ele = $('div.hljs, pre');
+    if (ele.length === 0) {
+      return 'copy-btn-dark';
+    }
+    var rgbArr = ele.css('background-color').replace(
+      /rgba*\(/, '').replace(')', '').split(',');
+    var color = 0.213 * rgbArr[0] + 0.715 * rgbArr[1] + 0.072 * rgbArr[2] > 255 / 2;
+    return color ? 'copy-btn-dark' : 'copy-btn-light';
+  }
+
+  var oldLoadCb = window.onload;
+  window.onload = function () {
+    oldLoadCb && oldLoadCb();
+
+    initCopyCode();
+  };
+}(window, document);
