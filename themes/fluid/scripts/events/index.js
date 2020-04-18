b169b98dad679b3936105f34976db69a0df6f59b
Royce2019 <1098870059@qq.com>
2020-04-18 18:25:51 +0800

    :sparkler: 增加代码 Lint 流

--- themes/fluid/scripts/events/index.js
+++ themes/fluid/scripts/events/index.js
@@ -1,7 +1,13 @@
+/* global hexo */
+
 'use strict';
 
 hexo.on('generateBefore', () => {
   require('./lib/merge-configs')(hexo);
   require('./lib/highlight')(hexo);
   require('./lib/lazyload')(hexo);
+});
+
+hexo.on('generateAfter', () => {
+  require('./lib/version')(hexo);
 });
