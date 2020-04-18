215c90cc0462e13a70bdfbfbf91070d95ae4166a
JalenChuh <JalenChuh@gmail.com>
2020-04-18 18:25:51 +0800

    :sparkler: 增加代码 Lint 流

--- themes/fluid/scripts/events/lib/lazyload.js
+++ themes/fluid/scripts/events/lib/lazyload.js
@@ -4,12 +4,12 @@ const joinPath = require('../../utils/join-path');
 
 module.exports = (hexo) => {
   var config = hexo.theme.config;
-  let loadingImage = config.lazyload.image;
+  let loadingImage = joinPath(joinPath(hexo.config.root, config.static_prefix.internal_img), 'loading.gif');
   if (!config.lazyload || !config.lazyload.enable || !loadingImage) {
     return;
   }
   if (config.lazyload.onlypost) {
-    hexo.extend.filter.register('after_post_render', function (page) {
+    hexo.extend.filter.register('after_post_render', function(page) {
       if (page.source.search(/^_posts\/.+\.md$/) === -1 && !page.lazyload) {
         return;
       }
@@ -17,7 +17,7 @@ module.exports = (hexo) => {
       return page;
     });
   } else {
-    hexo.extend.filter.register('after_render:html', function (str, data) {
+    hexo.extend.filter.register('after_render:html', function(str, data) {
       return lazyProcess(str, loadingImage);
     });
   }
@@ -28,6 +28,6 @@ const lazyProcess = (htmlContent, loadingImage) => {
     if (/srcset=/gi.test(str)) {
       return str;
     }
-    return str.replace(p1, `${ p1 }" srcset="${ loadingImage }`);
+    return str.replace(p1, `${p1}" srcset="${loadingImage}`);
   });
 };
